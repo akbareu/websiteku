b0640e2b25a80c60ab03807c4a82fdcab4ee9a84
akbareu <akbarbareu@gmail.com>
2021-03-15 23:36:18 +0700

    PR

--- firebase/admin.js
+++ firebase/admin.js
@@ -11,8 +11,10 @@
     };
     // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
+  database = firebase.database();
 
-// Autentikasi login Firebase
+
+// LOGIN SCRIPT
 firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
     // User is signed in.
@@ -49,59 +51,92 @@ function login(){
 function logout(){
   firebase.auth().signOut();
 }
+// END LOGIN SCRIPT
 
-var uploader = document.getElementById('uploader');
-var fileButton = document.getElementById('fileButton');
-var metadata = {
-  contentType: 'application/pdf'
-};
+// INBOX SCRIPT
+  var ref = database.ref('messages');
+  ref.on('value', gotData, errData);
 
-  fileButton.addEventListener('change', function(e) {
-    // File
-    var file = e.target.files[0];
-
-    // Buat Folder
-    var storageRef = firebase.storage().ref('eBook/' + file.name);
-
-    // Upload
-    var task = storageRef.put(file, metadata);
-
-    // Progres Upload
-    task.on('state_changed', 
-
-      function progress(snapshot) {
-        var percentage = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-        uploader.value = percentage;
-      },
-
-      function error(err) {
-        // Reset
-        document.getElementById('uploadCV').reset();
-
-        uploader.value = '0';
-        // Alert 
-        document.querySelector('.alert-danger').style.display = 'block';
-
-        // Alert hilang selama 2.9 detik
-        setTimeout(function(){
-        document.querySelector('.alert-danger').style.display = 'none';},2900);
-      },
-
-      function complete() {
-        task.snapshot.ref.getDownloadURL().then(function(downloadURL) {
-        window.alert('Copy URL:\n\n ' + downloadURL);
-        // Reset
-        document.getElementById('uploadCV').reset();
-
-        uploader.value = '0';
-        // Alert 
-        document.querySelector('.alert-success').style.display = 'block';
-
-        // Alert hilang selama 2.9 detik
-        setTimeout(function(){
-        document.querySelector('.alert-success').style.display = 'none';},2900);
-        });
+      function gotData(data) {
+        var messages = data.val();
+        var keys = Object.keys(messages);
+          console.log(keys);
+          for (var i = 0; i < keys.length; i++) {
+            var k = keys[i];
+            var hp     = messages[k].hp;
+            var nama   = messages[k].nama;
+            var pesan  = messages[k].pesan;
+            var subjek = messages[k].subjek;
+              // console.log(hp, nama, pesan, subjek);
+              var li = document.createElement('li');
+              li.classList.add("list-group-item", "list-group-item-action", "flex-column", "align-items-start");
+              var pesan = document.createTextNode('<div class="d-flex w-100 justify-content-between"><h5 class="mb-1">',subjek,'</h5></div><p class="mb-1">',pesan,'</p><small>',hp,'</small><br><small>',nama,'</small>');
+              li.appendChild(pesan);
+              document.getElementById("messageList").appendChild(li);
+              
+          }
       }
 
-    );
-  });
+      function errData(err) {
+        console.log('Errors');
+        console.log(err);
+      }
+// END INBOX SCRIPT
+
+// EBOOK STORE SCRIPT
+  var uploader = document.getElementById('uploader');
+  var fileButton = document.getElementById('fileButton');
+  var metadata = {
+    contentType: 'application/pdf'
+  };
+
+    fileButton.addEventListener('change', function(e) {
+      // File
+      var file = e.target.files[0];
+
+      // Buat Folder
+      var storageRef = firebase.storage().ref('eBook/' + file.name);
+
+      // Upload
+      var task = storageRef.put(file, metadata);
+
+      // Progres Upload
+      task.on('state_changed', 
+
+        function progress(snapshot) {
+          var percentage = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          uploader.value = percentage;
+        },
+
+        function error(err) {
+          // Reset
+          document.getElementById('uploadCV').reset();
+
+          uploader.value = '0';
+          // Alert 
+          document.querySelector('.alert-danger').style.display = 'block';
+
+          // Alert hilang selama 2.9 detik
+          setTimeout(function(){
+          document.querySelector('.alert-danger').style.display = 'none';},2900);
+        },
+
+        function complete() {
+          task.snapshot.ref.getDownloadURL().then(function(downloadURL) {
+          window.alert('Copy URL:\n\n ' + downloadURL);
+          // Reset
+          document.getElementById('uploadCV').reset();
+
+          uploader.value = '0';
+          // Alert 
+          document.querySelector('.alert-success').style.display = 'block';
+
+          // Alert hilang selama 2.9 detik
+          setTimeout(function(){
+          document.querySelector('.alert-success').style.display = 'none';},2900);
+          });
+        }
+
+      );
+    });
+// END EBOOK SCRIPT
